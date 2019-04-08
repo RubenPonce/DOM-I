8477075b77bcb7c8f7e7755907f2322dc9e5a5f0
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-08 14:01:59 -0700

    finished index.js assignment

--- js/index.js
+++ js/index.js
@@ -37,6 +37,44 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+//images
+const setImg = (id, path) =>{
+  const image = document.getElementById(id);
+  image.setAttribute('src', path);
+}
+setImg("logo-img",siteContent["nav"]["img-src"]);
+setImg("cta-img",siteContent["cta"]["img-src"]);
+setImg("middle-img",siteContent["main-content"]["middle-img-src"]);
+
+//header links
+const navLinks = Array.from(document.querySelectorAll("nav a"));
+navLinks.forEach((link,index) => link.textContent = siteContent["nav"][`nav-item-${index+1}`]);
+
+//cta-text
+const ctaText = document.querySelector(".cta-text");
+ctaText.firstElementChild.textContent = siteContent["cta"]["h1"];
+ctaText.lastElementChild.textContent = siteContent["cta"]["button"];
+
+//main-content
+const mainContentConstruct = (location,topOrBot,position)=>{
+  const content = document.querySelectorAll(`.main-content .${topOrBot} .text-content`)[position];
+  content.firstElementChild.textContent = siteContent["main-content"][`${location}-h4`];
+  content.lastElementChild.textContent = siteContent["main-content"][`${location}-content`];
+}
+mainContentConstruct("features",'top-content',0);
+mainContentConstruct("about","top-content",1);
+mainContentConstruct("services","bottom-content",0);
+mainContentConstruct("product","bottom-content",1);
+mainContentConstruct("vision","bottom-content",2);
+
+
+//contact section
+const contact = document.querySelector('.contact');
+[...contact.children].forEach((child,index)=> child.textContent = Object.values(siteContent.contact)[index]);
+const footer = document.querySelector('footer');
+footer.firstElementChild.textContent = siteContent["footer"]["copyright"];
+
+
+
+
+
